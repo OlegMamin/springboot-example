a117cd5b03175f9b0269c00ab197edf9062fc1ea
Oleg Mamin <maminoleg19@gmail.com>
2019-12-14 15:49:21 +0300

    Fix freeContract.ftl

--- src/main/java/com/mamin/spring/controllers/FirstController.java
+++ src/main/java/com/mamin/spring/controllers/FirstController.java
@@ -16,28 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by otherz on 12.12.2019.
  */
 @Controller
-@RestController
 public class FirstController {
 
     @Autowired
     private ContractsRepository contractsRepository;
 
-    @RequestMapping(path = "/hello")
+    @GetMapping(path = "/hello")
     public String showHelloWorld(){
         return "hello-world";
     }
-
-    @GetMapping(path = "/api/contracts/findFree")
-    public Page<Contract> getFreeContractsRest(
-            @RequestParam(defaultValue = "11") int size,
-            @RequestParam(defaultValue = "1") int page
-    ) {
-        return contractsRepository.findContractsToChosePagination(
-//                Sort.by("phoneNumber"),
-//                Sort.unsorted(),
-//                Sort.by("phoneNumber").descending(),
-//                Sort.by(Sort.Order.desc("phoneNumber"), Sort.Order.asc("clientName"))
-                PageRequest.of(page - 1, size, Sort.by("phoneNumber"))
-        );
+    @GetMapping(path = "/api/angularExample")
+    public String ang(){
+        return "freeContracts";
     }
+
 }
--- src/main/resources/templates/hello-world.ftl
+++ src/main/resources/templates/hello-world.ftl
@@ -1,5 +1,17 @@
 <html>
+<head>
+    <link rel="stylesheet" href="/css/tableStyle.css" />
+</head>
 <body>
+<table>
+    <thead>
+        <tr>
+            <td>
+                Hello in table
+            </td>
+        </tr>
+    </thead>
+</table>
 <h1>Hello world!!!</h1>
 </body>
 </html>
